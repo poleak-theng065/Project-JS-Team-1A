1bd3a78559bff4229c27942e6b38cc3f721d9a67
Leak Majestyy <poleaktheng2099@gmail.com>
2025-01-14 20:33:06 +0700

    "#5 set up database"

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// app.js
+
+// Import Firestore instance and required Firestore methods
+import { db } from "./firebase.js";
+import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+
